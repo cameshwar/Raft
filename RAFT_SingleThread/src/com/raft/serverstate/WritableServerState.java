5e13aacc4afb0f37b2215ba21475502f885b833b
cameshwar <cameshtech@gmail.com>
2015-01-31 19:40:05 +0530

    Changes for Follower state

--- RAFT_SingleThread/src/com/raft/serverstate/WritableServerState.java
+++ RAFT_SingleThread/src/com/raft/serverstate/WritableServerState.java
@@ -36,43 +36,50 @@ public class WritableServerState implements IServerStateContext{
 	}
 
 	@Override
-	public void changeState(ServerStateNode server) {
+	public void changeState(final ServerStateNode server) {
 		System.out.println("Writing");
-		SocketChannel client = null;
-		Selector selector = server.getSelector();
-		
-		try {
-			while(true) {
-			selector.select();
-			for (Iterator<SelectionKey> i = selector.selectedKeys().iterator(); i.hasNext();) {
-				SelectionKey key = i.next(); 
-				i.remove();
-				
-				client = (SocketChannel) key.channel();					
-				
-				
-				//ReadableByteChannel channel = Channels.newChannel(client.socket().getInputStream());
-				client = (SocketChannel) key.channel();
-				ByteBuffer buf = null;										
-				buf = ByteBuffer.wrap(Integer.toString(5).getBytes());
-				client.write(buf);
-				this.ready = true;
-				//key.interestOps(SelectionKey.OP_READ);
-				//MachineState.setServerState(EServerState.READ);
-				/*if(MachineState.serverState!=EServerState.WRITE) {
-					System.out.println("key set");
-					key.interestOps(SelectionKey.OP_READ);
-					break;
-				}*/
-			}
-			if(this.ready)
-				break;
-			
-		}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		new Thread(
+				new Runnable() {
+					
+					@Override
+					public void run() {
+						SocketChannel client = null;
+						Selector selector = server.getSelector();
+						
+						try {
+							while(true) {
+							selector.select();
+							for (Iterator<SelectionKey> i = selector.selectedKeys().iterator(); i.hasNext();) {
+								SelectionKey key = i.next(); 
+								i.remove();
+								
+								client = (SocketChannel) key.channel();					
+								
+								
+								//ReadableByteChannel channel = Channels.newChannel(client.socket().getInputStream());
+								client = (SocketChannel) key.channel();
+								ByteBuffer buf = null;										
+								buf = ByteBuffer.wrap(Integer.toString(5).getBytes());
+								client.write(buf);
+								ready = true;
+								//key.interestOps(SelectionKey.OP_READ);
+								//MachineState.setServerState(EServerState.READ);
+								/*if(MachineState.serverState!=EServerState.WRITE) {
+									System.out.println("key set");
+									key.interestOps(SelectionKey.OP_READ);
+									break;
+								}*/
+							}
+							if(ready)
+								break;
+							
+						}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}).start();
 		
 	}
 
